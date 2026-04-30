3471c65616631cbf5b6b79ed725e93ee10ee72c7
Rihan Sabri <mrihansabri@gmail.com>
2026-05-01 03:34:59 +0530

    Fixed default route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ function App() {
 
       <Routes>
 
+        <Route path="/" element={<Login />} />
+        
         <Route path="/login" element={<Login />} />
 
         <Route path="/register" element={<Register />} />
