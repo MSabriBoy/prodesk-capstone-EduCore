00b10f7c341aa80d277958760ad738b818e8e547
Rihan Sabri <mrihansabri@gmail.com>
2026-05-01 03:25:53 +0530

    cors error corrected

--- server/server.js
+++ server/server.js
@@ -12,7 +12,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
