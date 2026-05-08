aea5f55181d03daab2f434192131edfd48e52eac
Rihan Sabri <mrihansabri@gmail.com>
2026-05-08 20:08:26 +0530

    basic UI of login page has been implemented

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,21 +1,16 @@
 import { useState } from "react";
-
 import axios from "axios";
-
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, Link } from "react-router-dom";
 
 function Login() {
 
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
-
   const [password, setPassword] = useState("");
 
 
   const handleLogin = async () => {
-
     try {
 
       const response = await axios.post(
@@ -26,7 +21,6 @@ function Login() {
         }
       );
 
-      // Save JWT token
       localStorage.setItem(
         "token",
         response.data.token
@@ -38,37 +32,53 @@ function Login() {
 
     } catch (error) {
 
-      alert(error.response.data.message);
+      alert(error.response?.data?.message || "Login failed");
 
     }
-
   };
 
 
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
 
-      <h1>Login</h1>
+      <div className="bg-white shadow-lg rounded-2xl p-8 w-full max-w-md">
 
-      <input
-        type="email"
-        placeholder="Enter Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
+        <h1 className="text-3xl font-bold text-center mb-6">
+          EduCore Login
+        </h1>
 
-      <br /><br />
+        <input
+          type="email"
+          placeholder="Enter Email"
+          className="w-full border p-3 rounded-lg mb-4"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <input
-        type="password"
-        placeholder="Enter Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          className="w-full border p-3 rounded-lg mb-4"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-      <br /><br />
+        <button
+          onClick={handleLogin}
+          className="w-full bg-black text-white py-3 rounded-lg"
+        >
+          Login
+        </button>
 
-      <button onClick={handleLogin}>
-        Login
-      </button>
+        <p className="text-center mt-4">
+          New here?{" "}
+          <Link
+            to="/register"
+            className="font-semibold"
+          >
+            Register
+          </Link>
+        </p>
+
+      </div>
 
     </div>
   );
