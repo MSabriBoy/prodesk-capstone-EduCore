2bc8f2a16b2ed3b46bb3038f1735007c2f2e1445
Rihan Sabri <mrihansabri@gmail.com>
2026-05-08 19:27:20 +0530

    courseRoute has been updated and add the update and delete route.

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -5,15 +5,22 @@ const router = express.Router();
 const protectRoute = require("../middleware/authMiddleware");
 
 const {
-  createCourse,
-  getCourses
+    createCourse,
+    getCourses,
+    updateCourse,
+    deleteCourse
 } = require("../controllers/courseController");
 
 
 // CREATE + READ
 router.route("/")
-  .post(protectRoute, createCourse)
-  .get(protectRoute, getCourses);
+    .post(protectRoute, createCourse)
+    .get(protectRoute, getCourses);
+
+// UPDATE + DELETE
+router.route("/:id")
+    .put(protectRoute, updateCourse)
+    .delete(protectRoute, deleteCourse);
 
 
 module.exports = router;
