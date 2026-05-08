c726726139e5adb8281f39612bc2f5f8fb0a883d
Rihan Sabri <mrihansabri@gmail.com>
2026-05-09 04:35:39 +0530

    Enhanced the ui of paymentsuccess page

--- client/src/pages/PaymentSuccess.jsx
+++ client/src/pages/PaymentSuccess.jsx
@@ -1,28 +1,111 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import {
+  useSearchParams,
+  useNavigate
+} from "react-router-dom";
+
+import API from "../api";
 
 
 function PaymentSuccess() {
 
+  const navigate = useNavigate();
+
+  const [searchParams] =
+    useSearchParams();
+
+  const [receipt, setReceipt] =
+    useState(null);
+
+
+  useEffect(() => {
+
+    fetchReceipt();
+
+  }, []);
+
+
+  const fetchReceipt = async () => {
+
+    try {
+
+      const sessionId =
+        searchParams.get(
+          "session_id"
+        );
+
+      const response =
+        await API.get(
+          `/api/payment/session/${sessionId}`
+        );
+
+      setReceipt(
+        response.data.session
+      );
+
+    } catch (error) {
+
+      console.log(error);
+
+    }
+
+  };
+
+
   return (
 
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
 
-      <div className="bg-white shadow-lg rounded-2xl p-8 text-center">
+      <div className="bg-white shadow-lg rounded-2xl p-8 w-full max-w-lg">
 
-        <h1 className="text-3xl font-bold text-green-600 mb-4">
-          Payment Successful 🎉
+        <h1 className="text-3xl font-bold text-green-600 text-center mb-6">
+          EduCore Receipt 🎉
         </h1>
 
-        <p className="text-gray-600 mb-6">
-          Course purchased successfully.
-        </p>
+        {receipt && (
 
-        <Link
-          to="/dashboard"
-          className="bg-black text-white px-6 py-3 rounded-lg"
+          <div className="space-y-3">
+
+            <p>
+              <strong>Transaction ID:</strong>{" "}
+              {receipt.id}
+            </p>
+
+            <p>
+              <strong>Course:</strong>{" "}
+              {receipt.metadata.courseName}
+            </p>
+
+            <p>
+              <strong>Amount Paid:</strong>{" "}
+              ₹{receipt.amount_total / 100}
+            </p>
+
+            <p>
+              <strong>Status:</strong>{" "}
+              {receipt.payment_status}
+            </p>
+
+            <p>
+              <strong>Date:</strong>{" "}
+              {new Date(
+                receipt.created * 1000
+              ).toLocaleString()}
+            </p>
+
+          </div>
+
+        )}
+
+        <button
+          onClick={() =>
+            navigate("/dashboard")
+          }
+          className="w-full mt-6 bg-black text-white py-3 rounded-lg"
         >
           Back to Dashboard
-        </Link>
+        </button>
 
       </div>
 
