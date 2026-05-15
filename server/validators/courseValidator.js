fda1552f0bcf256030d28adddbd9a718bce31ecf
Rihan Sabri <mrihansabri@gmail.com>
2026-05-15 11:58:58 +0530

    Add course request validation schema

---
+++ server/validators/courseValidator.js
@@ -0,0 +1,9 @@
+const Joi = require("joi");
+
+const courseSchema = Joi.object({
+  title: Joi.string().min(3).required(),
+  description: Joi.string().min(5).required(),
+  price: Joi.number().min(1).required()
+});
+
+module.exports = courseSchema;
